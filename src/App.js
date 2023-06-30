d2ea02a030a56d137dc27c174f50a39790527b3c
steventhomasdev <steventhomasdev@gmail.com>
2023-06-30 15:50:11 -0400

    removed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import ReactDOM from 'react-dom'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { faTwitter, faFontAwesome } from '@fortawesome/free-brands-svg-icons'
